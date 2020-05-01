b3a1ac8b5b54b053225a96b4b3cdf37fa401c4db
Garnett05 <charlleslimamota10@hotmail.com>
2020-05-01 15:08:35 -0300

    MVVM - GamePage - Initial Structure

--- AppGuessThePlayer/AppGuessThePlayer/ViewModel/GameViewModel.cs
+++ AppGuessThePlayer/AppGuessThePlayer/ViewModel/GameViewModel.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace AppGuessThePlayer.ViewModel
 {
-    class GameViewModel
+    public class GameViewModel
     {
+        public byte PlayerScore { get; set; }
+        public string Player { get; set; }        
+        public bool ContainerCountdown { get; set; }
+        public bool ContainerStart { get; set; }
+        public string CountdownPlayer { get; set; }
+        public Command ShowPlayer { get; set; }
+        public Command RightAnswer { get; set; }
+        public Command WrongAnswer { get; set; }
+        public Command StartGame { get; set; }
+
+        public GameViewModel()
+        {
+            ContainerCountdown = false;
+            ShowPlayer = new Command();
+            RightAnswer = new Command();
+            WrongAnswer = new Command();
+            StartGame = new Command();
+        }
+
     }
 }
