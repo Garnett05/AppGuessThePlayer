5f572f2e3698afe9030e817c242b9c1f63e3d972
Garnett05 <charlleslimamota10@hotmail.com>
2020-05-02 16:51:41 -0300

    GamePage - Value for every answer - Adjusting with a converter class

---
+++ AppGuessThePlayer/AppGuessThePlayer/View/Util/Converter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AppGuessThePlayer.View.Util
+{
+    public class Converter : IValueConverter
+    {
+        //View -> ViewModel
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            short score = short.Parse(value.ToString());
+            if(score == 0)
+            {
+                return "";
+            }
+            else
+            {
+                return "Player answer value: " + score;
+            }            
+        }
+
+        //ViewModel -> View
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
